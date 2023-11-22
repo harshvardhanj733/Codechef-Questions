2a09342dfd799c7741c3ae8e55805108cab0c405
harshvardhanj733 <harshvardhanj733@gmail.com>
2023-11-22 12:15:02 +0530

    Longest Repeating Character Replacement (Sliding Window) solved

--- 424.cpp
+++ 424.cpp
@@ -1,27 +1,52 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        map<char, int> freq;
-        int firstPointer = 0, secondPointer = 0;
-        int maxFreq = 0, maxLength = -1;
+        // map<char, int> freq;
+        // int firstPointer = 0, secondPointer = 0;
+        // int maxFreq = 0, maxLength = -1;
 
-        while(secondPointer < s.size()){
-            freq[s[secondPointer]]++;
-            maxFreq = max(maxFreq, freq[s[secondPointer]]);
+        // while(secondPointer < s.size()){
+        //     freq[s[secondPointer]]++;
+        //     maxFreq = max(maxFreq, freq[s[secondPointer]]);
             
-            if((secondPointer-firstPointer+1) - maxFreq > k){
-                freq[s[firstPointer]]--;
-                firstPointer++;
-            }
+        //     if((secondPointer-firstPointer+1) - maxFreq > k){
+        //         freq[s[firstPointer]]--;
+        //         firstPointer++;
+        //     }
 
-            int length = secondPointer-firstPointer+1;
-            maxLength = max(length, maxLength);
-            secondPointer++;
+        //     int length = secondPointer-firstPointer+1;
+        //     maxLength = max(length, maxLength);
+        //     secondPointer++;
+        // }
+
+        // return maxLength; 
+
+
+        int l = 0, r = 0, maxF = 0, len  = 0;
+        map<int, int> count;
+
+        while(r < s.size()){
+            count[s[r]]++;
+            maxF = max(maxF, count[s[r]]);
+            if(r - l + 1 - maxF <= k){
+                len = max(len, r - l + 1);
+            }
+            else{
+                while(r - l + 1 - maxF > k){
+                    count[s[l]]--;
+                    // maxF = 0;
+                    // for(auto i: count){
+                    //     maxF = max(maxF, i.second);
+                    // }
+                    l++;
+                }
+            }
+            r++;
         }
 
-        return maxLength;
+        return len;
     }
 };
 
-//Time Complexity: O(n);
-//Space Complexity: O(26);
+//TC: O(N);
+//SC: O(26);
