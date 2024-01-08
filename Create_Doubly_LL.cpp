e2e1b023f9dfe6baf712d5c476b470b7841e5839
harshvardhanj733 <harshvardhanj733@gmail.com>
2024-01-08 21:27:18 +0530

    Create Doubly Linked List from Array solved

---
+++ Create_Doubly_LL.cpp
@@ -0,0 +1,29 @@
+/*
+ * Definition for doubly-linked list.
+ * class Node
+ * {
+ * public:
+ *    int data;
+ *    Node *next, *prev;
+ *    Node() : data(0), next(nullptr), prev(nullptr) {}
+ *    Node(int x) : data(x), next(nullptr), prev(nullptr) {}
+ *    Node(int x, Node *next, Node *prev) : data(x), next(next), prev(prev) {}
+ * };
+ */
+
+Node* constructDLL(vector<int>& arr) {
+    // Write your code here
+    Node* head = new Node(arr[0], NULL, NULL);
+    Node* curr = head;
+    for(int i = 1; i < arr.size(); i++){
+        Node* temp = new Node(arr[i], NULL, NULL);
+        curr->next = temp;
+        temp->prev = curr;
+        curr = curr->next;
+    }
+
+    return head;
+}
+
+//TC: O(n);
+//SC: O(1);
